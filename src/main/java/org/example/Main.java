29d426d25258a2fbc2032f8847b2548c7c634403
Alexander Pukhalskyi <p.vadim974@gmail.com>
2022-11-13 17:03:12 +0200

    Add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,10 +10,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        //task1
         List<Integer> list = new ArrayList<>(List.of(5, -2, 4, 10, -22, 35, -14, 45));
         Array array = new Array();
         System.out.println(array.sortArray(list));
 
+        //task2
         ArrayList<String> secondList2 = new ArrayList<>();
         secondList2.add("#Hello wdadwa #wda #Hello #Helflo #Helfdalo");
         secondList2.add("awdgr #Heldwalo wdadwa #wda #Helflo");
@@ -21,6 +23,7 @@ public class Main {
         Tag2 teg2 = new Tag2();
         System.out.println(teg2.TopTeg(secondList2));
 
+        //task3
         Collection col = new ArrayList();
         col.add("Cube");
         col.add("Bullet");
